1ad10f9afc2af952df12345027528ac16daa8330
adrian <a_adrian.camara.munoz@iespablopicasso.es>
2021-11-18 14:15:27 +0100

    añado programa java

---
+++ ArraysAleatorios.java
@@ -0,0 +1,12 @@
+package Calculadora;
+
+public class ArraysAleatorios {
+	public static void main(String[] args) {
+		int[] array = new int[20];
+		
+		for(int i = 0; i < array.length; i++) {
+			array[i] = ((int) (Math.random()*10));
+		}
+	
+	}
+}
